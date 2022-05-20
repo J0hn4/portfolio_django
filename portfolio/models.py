1ec380740b8e78db9b4a1b8ea9e814aaabca994e
ajax <prof_x22@protonmail.com>
2022-05-20 12:01:51 +0200

    May 20th, 2022

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,5 +7,8 @@ class Project(models.Model):
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
 
+    def __str__(self):
+        return self.title
+
 
 
